bcc6a5cd9592b15d8563c5421145537fe06f2747
Thomas Aubry <t.aubry@samsung.com>
2019-11-11 15:41:42 +0100

    setup.py compile in release mode and the tests also do the benchmark

--- python-bindings/tests/test_k_means.py
+++ python-bindings/tests/test_k_means.py
@@ -5,15 +5,21 @@ import numpy as np
 
 import pytest
 from linfa_k_means import KMeans
-
+from sklearn.cluster import KMeans as sk_KMeans
 
 @pytest.fixture(scope="session", autouse=True)
 def make_data():
-    return make_blobs()
+    return make_blobs(n_samples=1000000)
 
 
 def test_k_means_rust(benchmark, make_data):
     dataset, cluster_index = make_data
-    model = KMeans(3)
-    labels = model.fit_predict(dataset)
+    model = KMeans(3, max_iter=100)
+    labels = benchmark(model.fit_predict, dataset)
+    assert len(labels) == len(cluster_index)
+
+def test_k_means_python(benchmark, make_data):
+    dataset, cluster_index = make_data
+    model = sk_KMeans(3, init="random", algorithm="full", max_iter=100)
+    labels = benchmark(model.fit_predict, dataset)
     assert len(labels) == len(cluster_index)
