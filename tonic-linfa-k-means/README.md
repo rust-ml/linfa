7830e46ad74432d38ffdc026e615d683fccf7bf8
Maxim Vorobjov <maxim.vorobjov@gmail.com>
2019-11-12 11:33:19 +0100

    Test data and test data generators (#4)
    * server with centroids
    
    * no clone
    
    * no arc anymore
    
    * data generators and benchmark example

--- tonic-linfa-k-means/README.md
+++ tonic-linfa-k-means/README.md
@@ -1,7 +1,7 @@
 # Run server
 
 ```
-cargo run -- --load-centroids=test/centroids.json
+cargo run --release -- --load-centroids=../test/centroids.json
 ```
 
 # Run client
@@ -10,3 +10,20 @@ cargo run -- --load-centroids=test/centroids.json
 cargo run --bin client
 ```
 
+# Benchmark 
+
+```
+ghz --proto=protos/centroids.proto --call=ml.ClusteringService.Predict --insecure --data-file=./test/observations.json localhost:8000
+```
+
+# Generate samples
+
+```
+cargo run --bin gentestdata -- --features=2 --samples=10000
+```
+
+# Generate centroids
+
+```
+cargo run --bin gencentroiddata -- --features=2 --centroids=100
+```
--- tonic-linfa-k-means/src/bin/gentestdata.rs
+++ tonic-linfa-k-means/src/bin/gentestdata.rs
@@ -1,7 +1,32 @@
-use ndarray::array;
 use serde_json;
+use structopt::StructOpt;
+use rand::{self, Rng};
+use serde::{Serialize};
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(short = "f", long="features", default_value = "2")]
+    features: usize,
+    #[structopt(short = "s", long="samples", default_value = "1000")]
+    samples: usize
+}
+
+#[derive(Serialize)]
+struct Sample {
+    features: Vec<f64>
+}
+
+impl Sample {
+    pub fn new(features: Vec<f64>) -> Self {
+        Self { features }
+    }
+}
 
 fn main() {
-    let expected_centroids = array![[10., 10.], [1., 12.], [20., 30.], [-20., 30.],];
-    println!("{}", serde_json::to_string(&expected_centroids).unwrap());
+    let opt = Opt::from_args();
+    let mut rng = rand::thread_rng();
+    let gen_sample = |_| (0..opt.features).map(|_| rng.gen_range(-100.0, 100.0)).collect::<Vec<f64>>();
+
+    let samples = (0..opt.samples).map(gen_sample).map(Sample::new).collect::<Vec<Sample>>();
+    println!("{}", serde_json::to_string(&samples).unwrap());
 }
