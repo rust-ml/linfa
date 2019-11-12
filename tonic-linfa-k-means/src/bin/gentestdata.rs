30e49751900c8c5ae19c8e8cdcb12a46baf5d83f
Maxim Vorobjov <maxim.vorobjov@gmail.com>
2019-11-12 15:53:35 +0100

    Batch processing on server (#6)
    * refactor to accomodate KMeans API
    
    * batch mode for server with test data

--- tonic-linfa-k-means/src/bin/gentestdata.rs
+++ tonic-linfa-k-means/src/bin/gentestdata.rs
@@ -4,24 +4,42 @@ use ndarray::{Array, Array1};
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 use serde::{Serialize};
+use rand::Rng;
 
 #[derive(StructOpt)]
 struct Opt {
     #[structopt(short = "f", long="features", default_value = "2")]
     features: usize,
     #[structopt(short = "s", long="samples", default_value = "1000")]
-    samples: usize
+    samples: usize,
+    /// In the batch mode will generate batches number of samples of different size
+    batch: bool,
+    #[structopt(short = "b", long="batches", default_value = "1000")]
+    batches: usize
 }
 
 #[derive(Serialize)]
-struct Sample {
+struct Observation {
     features: Vec<f64>
 }
 
-impl Sample {
+impl Observation {
     pub fn new(features: Array1<f64>) -> Self {
         Self { 
             features: features.to_vec()
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct Batch {
+    observations: Vec<Observation>
+}
+
+impl Batch {
+    pub fn new(observations: Vec<Observation>) -> Self {
+        Self { 
+            observations
         }
     }
 }
@@ -31,9 +49,22 @@ fn main() {
     let distr = Uniform::new(-100.0, 100.0);
     let gen_sample = |_| Array::random(opt.features, distr);
 
-    let samples = (0..opt.samples)
+    let gen_samples = |n_samples| (0..n_samples)
         .map(gen_sample)
-        .map(Sample::new)
-        .collect::<Vec<Sample>>();
-    println!("{}", serde_json::to_string(&samples).unwrap());
+        .map(Observation::new)
+        .collect::<Vec<Observation>>();
+    
+    if !opt.batch {
+        println!("{}", serde_json::to_string(&gen_samples(opt.samples)).unwrap());
+    } else {
+        let mut rng = rand::thread_rng();
+
+        let batches = 
+            (0..opt.batches)
+                .map(|_| rng.gen_range(1, opt.samples))
+                .map(gen_samples)
+                .map(Batch::new)
+                .collect::<Vec<Batch>>();
+        println!("{}", serde_json::to_string(&batches).unwrap());
+    }
 }
--- tonic-linfa-k-means/src/server.rs
+++ tonic-linfa-k-means/src/server.rs
@@ -7,7 +7,10 @@ pub mod centroids {
     tonic::include_proto!("ml");
 }
 
-use centroids::{server::ClusteringService, PredictRequest, PredictResponse};
+use centroids::{server::ClusteringService, 
+    PredictRequest, PredictResponse,
+    PredictBatchRequest, PredictBatchResponse,
+};
 
 pub use centroids::server::ClusteringServiceServer;
 
@@ -38,10 +41,29 @@ impl ClusteringService for KMeansProto {
 
         if let Some(cluster_index) = self.store.kmeans.predict(&observation).get(0) {
             Ok(Response::new(
-                PredictResponse { cluster_index: *cluster_index as i32 })
-            )
+                PredictResponse { cluster_index: *cluster_index as i32 }
+            ))
         } else {
             Err(Status::new(Code::Internal, "KMeans::predict did not return value"))
         }
+    }
+
+    async fn predict_batch(
+        &self,
+        request: Request<PredictBatchRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<PredictBatchResponse>, Status> {
+        // Return an instance of type HelloReply
+        //dbg!("Got a request: {:?}", &request);
+
+        let observations = request.into_inner().observations;
+    
+        let cluster_indexes = observations.into_iter()
+            .map(|obs| Array::from_shape_vec((1, obs.features.len()), obs.features).unwrap() )
+            .map(|observation| *self.store.kmeans.predict(&observation).get(0).unwrap() as i32)
+            .collect::<Vec<i32>>();
+
+        Ok(Response::new(
+            PredictBatchResponse { cluster_indexes  }
+        ))
     }
 }
