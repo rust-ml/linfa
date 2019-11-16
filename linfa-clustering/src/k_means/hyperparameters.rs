10ddf8c0f0c76ce4f9259ab66706ac11d97f084b
Luca Palmieri <lpalmieri@truelayer.com>
2019-11-16 17:37:52 +0000

    Refactor to avoid having pub fields on KMeansHyperparams - use getter methods

--- linfa-clustering/src/k_means/hyperparameters.rs
+++ linfa-clustering/src/k_means/hyperparameters.rs
@@ -7,13 +7,13 @@ pub struct KMeansHyperParams {
     /// The training is considered complete if the euclidean distance
     /// between the old set of centroids and the new set of centroids
     /// after a training iteration is lower or equal than `tolerance`.
-    pub tolerance: f64,
+    tolerance: f64,
     /// We exit the training loop when the number of training iterations
     /// exceeds `max_n_iterations` even if the `tolerance` convergence
     /// condition has not been met.
-    pub max_n_iterations: u64,
+    max_n_iterations: u64,
     /// The number of clusters we will be looking for in the training dataset.
-    pub n_clusters: usize,
+    n_clusters: usize,
 }
 
 /// An helper struct used to construct a set of [valid hyperparameters](struct.KMeansHyperParams.html) for
@@ -75,6 +75,25 @@ impl KMeansHyperParams {
             max_n_iterations: 300,
             n_clusters,
         }
+    }
+
+    /// The training is considered complete if the euclidean distance
+    /// between the old set of centroids and the new set of centroids
+    /// after a training iteration is lower or equal than `tolerance`.
+    pub fn tolerance(&self) -> f64 {
+        self.tolerance
+    }
+
+    /// We exit the training loop when the number of training iterations
+    /// exceeds `max_n_iterations` even if the `tolerance` convergence
+    /// condition has not been met.
+    pub fn max_n_iterations(&self) -> u64 {
+        self.max_n_iterations
+    }
+
+    /// The number of clusters we will be looking for in the training dataset.
+    pub fn n_clusters(&self) -> usize {
+        self.n_clusters
     }
 
     fn build(n_clusters: usize, tolerance: f64, max_n_iterations: u64) -> Self {
