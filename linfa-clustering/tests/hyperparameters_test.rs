4ad76862558bcb580dc79df43a64ca688fd323cc
Luca Palmieri <lpalmieri@truelayer.com>
2019-11-20 10:23:06 +0000

    Formatting

--- linfa-clustering/tests/hyperparameters_test.rs
+++ linfa-clustering/tests/hyperparameters_test.rs
@@ -9,17 +9,13 @@ fn n_clusters_cannot_be_zero() {
 #[test]
 #[should_panic]
 fn tolerance_has_to_positive() {
-    KMeansHyperParams::new(1)
-        .tolerance(-1.)
-        .build();
+    KMeansHyperParams::new(1).tolerance(-1.).build();
 }
 
 #[test]
 #[should_panic]
 fn tolerance_cannot_be_zero() {
-    KMeansHyperParams::new(1)
-        .tolerance(0.)
-        .build();
+    KMeansHyperParams::new(1).tolerance(0.).build();
 }
 
 #[test]
