d7f8236c8a183c368cf4c38567827f45773b32a2
xd009642 <danielmckenna93@gmail.com>
2019-12-26 14:31:40 +0000

    Update doc comments for HyperParams

--- linfa-clustering/src/dbscan/hyperparameters.rs
+++ linfa-clustering/src/dbscan/hyperparameters.rs
@@ -18,17 +18,27 @@ pub struct DbscanHyperParamsBuilder {
 }
 
 impl DbscanHyperParamsBuilder {
+    /// Distance between points for them to be considered neighbours.
     pub fn tolerance(mut self, tolerance: f64) -> Self {
         self.tolerance = tolerance;
         self
     }
 
+    /// Return an instance of `DbscanHyperParams` after having performed
+    /// validation checks on all hyperparameters.
+    ///
+    /// **Panics** if any of the validation checks fail.
     pub fn build(self) -> DbscanHyperParams {
         DbscanHyperParams::build(self.tolerance, self.min_points)
     }
 }
 
 impl DbscanHyperParams {
+    /// Minimum number of neighboring points a point needs to have to be a core
+    /// point and not a noise point.
+    ///
+    /// Defaults are provided if the optional parameters are not specified:
+    /// * `tolerance = 1e-4`
     pub fn new(min_points: usize) -> DbscanHyperParamsBuilder {
         DbscanHyperParamsBuilder {
             min_points,
@@ -36,10 +46,14 @@ impl DbscanHyperParams {
         }
     }
 
+    /// Two points are considered neighbors if the euclidean distance between
+    /// them is below the tolerance
     pub fn tolerance(&self) -> f64 {
         self.tolerance
     }
 
+    /// Minimum number of a points in a neighborhood around a point for it to
+    /// not be considered noise
     pub fn minimum_points(&self) -> usize {
         self.min_points
     }
