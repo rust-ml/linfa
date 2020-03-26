bae05a22e79b6b4d56861398fe3093651c05aebf
leaen <mossbanay@gmail.com>
2020-03-26 21:23:59 +1100

    Swap to faster method of calculating class frequencies in splits

--- linfa-trees/src/decision_trees/algorithm.rs
+++ linfa-trees/src/decision_trees/algorithm.rs
@@ -1,5 +1,80 @@
 use crate::decision_trees::hyperparameters::{DecisionTreeParams, SplitQuality};
-use ndarray::{Array, Array1, ArrayBase, Data, Ix1, Ix2};
+use ndarray::{s, Array, Array1, ArrayBase, Data, Ix1, Ix2};
+
+struct SplitCandidate {
+    split_value: f64,
+    left_class_freq: Vec<u64>,
+    right_class_freq: Vec<u64>,
+}
+
+struct SplitIterator {
+    left_idxs: Vec<usize>,
+    right_idxs: Vec<usize>,
+    left_class_freq: Vec<u64>,
+    right_class_freq: Vec<u64>,
+    feature: Array1<f64>,
+    labels: Array1<u64>,
+}
+
+impl SplitIterator {
+    fn new(
+        feature: Array1<f64>,
+        labels: Array1<u64>,
+        row_idxs: &Vec<usize>,
+        n_classes: u64,
+    ) -> SplitIterator {
+        let mut feature_pairs = vec![];
+        for idx in row_idxs.iter() {
+            feature_pairs.push((*idx, &feature[*idx]));
+        }
+
+        feature_pairs.sort_by(|a, b| b.1.partial_cmp(a.1).unwrap_or(std::cmp::Ordering::Less));
+
+        let left_idxs = feature_pairs.iter().map(|a| a.0).collect();
+        let right_idxs = vec![];
+
+        let left_class_freq = class_frequencies(&labels, &row_idxs, n_classes);
+        let right_class_freq = vec![0; n_classes as usize];
+
+        SplitIterator {
+            left_idxs: left_idxs,
+            right_idxs: right_idxs,
+            left_class_freq: left_class_freq,
+            right_class_freq: right_class_freq,
+            feature: feature,
+            labels: labels,
+        }
+    }
+}
+
+impl Iterator for SplitIterator {
+    type Item = SplitCandidate;
+
+    fn next(&mut self) -> Option<SplitCandidate> {
+        if self.left_idxs.len() < 2 {
+            None
+        } else {
+            self.right_idxs.push(self.left_idxs.pop().unwrap());
+            let swapped_class = self.labels[*self.left_idxs.last().unwrap()];
+            self.left_class_freq[swapped_class as usize] -= 1;
+            self.right_class_freq[swapped_class as usize] += 1;
+
+            let left_idx = self.left_idxs.last().unwrap();
+            let right_idx = self.right_idxs.last().unwrap();
+
+            let left_value = self.feature[*left_idx];
+            let right_value = self.feature[*right_idx];
+
+            let split_value = (left_value + right_value) as f64 / 2.0;
+
+            Some(SplitCandidate {
+                split_value: split_value,
+                left_class_freq: self.left_class_freq.to_vec(),
+                right_class_freq: self.right_class_freq.to_vec(),
+            })
+        }
+    }
+}
 
 struct TreeNode {
     feature_idx: usize,
@@ -32,38 +107,37 @@ impl TreeNode {
         let mut best_split_value = None;
         let mut best_score = None;
 
+        let parent_class_freq = class_frequencies(&y, row_idxs, hyperparameters.n_classes);
+
         // 1. Find best split for current level
         for feature_idx in 0..(x.ncols()) {
-            let split_values = split_values_for_feature(&x, &row_idxs, feature_idx);
+            let feature = x.slice(s![.., feature_idx]).to_owned();
+            let labels = y.to_owned();
 
-            for sv in split_values {
-                let (left_idxs, right_idxs) =
-                    split_on_feature_by_value(&x, &row_idxs, feature_idx, sv);
-
-                if left_idxs.len() < hyperparameters.min_samples_leaf as usize {
-                    continue;
-                }
-
-                if right_idxs.len() < hyperparameters.min_samples_leaf as usize {
-                    continue;
-                }
-
+            for split_candidate in
+                SplitIterator::new(feature, labels, row_idxs, hyperparameters.n_classes)
+            {
                 let (left_score, right_score) = match hyperparameters.split_quality {
                     SplitQuality::Gini => (
-                        -gini_impurity(&y, &left_idxs, hyperparameters.n_classes),
-                        -gini_impurity(&y, &right_idxs, hyperparameters.n_classes),
+                        -gini_impurity(&split_candidate.left_class_freq),
+                        -gini_impurity(&split_candidate.right_class_freq),
                     ),
                     SplitQuality::Entropy => (
-                        information_gain(&y, &row_idxs, &left_idxs, hyperparameters.n_classes),
-                        information_gain(&y, &row_idxs, &right_idxs, hyperparameters.n_classes),
+                        information_gain(&parent_class_freq, &split_candidate.left_class_freq),
+                        information_gain(&parent_class_freq, &split_candidate.right_class_freq),
                     ),
                 };
 
-                let score = (left_score + right_score) / 2.0;
+                let left_weight: f64 = split_candidate.left_class_freq.iter().sum::<u64>() as f64
+                    / row_idxs.len() as f64;
+                let right_weight: f64 = split_candidate.right_class_freq.iter().sum::<u64>() as f64
+                    / row_idxs.len() as f64;
+
+                let score = left_weight * left_score + right_weight * right_score;
 
                 if best_score.is_none() || score > best_score.unwrap() {
                     best_feature_idx = Some(feature_idx);
-                    best_split_value = Some(sv);
+                    best_split_value = Some(split_candidate.split_value);
                     best_score = Some(score);
                 }
             }
@@ -200,34 +274,6 @@ fn split_on_feature_by_value(
     (left, right)
 }
 
-fn split_values_for_feature(
-    x: &ArrayBase<impl Data<Elem = f64> + Sync, Ix2>,
-    row_idxs: &Vec<usize>,
-    feature_idx: usize,
-) -> Vec<f64> {
-    let mut values: Vec<f64> = vec![];
-
-    for idx in row_idxs.iter() {
-        values.push(*x.get((*idx, feature_idx)).unwrap());
-    }
-
-    values.sort_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Less));
-
-    let mut split_values = vec![];
-    let mut last_value = None;
-
-    for v in values.iter() {
-        if let Some(lv) = last_value {
-            if lv != v {
-                split_values.push((lv + v) / 2.0)
-            }
-        }
-        last_value = Some(v);
-    }
-
-    split_values
-}
-
 /// Given an array of labels and a mask `row_idxs` calculate the frequency of
 /// each class from 0 to `n_classes-1`.
 fn class_frequencies(
@@ -278,15 +324,9 @@ fn prediction_for_rows(
 
 /// Given an array of labels and a mask `row_idxs` calculate the gini impurity
 /// of the subset.
-fn gini_impurity(
-    labels: &ArrayBase<impl Data<Elem = u64> + Sync, Ix1>,
-    row_idxs: &Vec<usize>,
-    n_classes: u64,
-) -> f64 {
-    let n_samples = row_idxs.len();
+fn gini_impurity(class_freq: &Vec<u64>) -> f64 {
+    let n_samples: u64 = class_freq.iter().sum();
     assert!(n_samples > 0);
-
-    let class_freq = class_frequencies(labels, row_idxs, n_classes);
 
     let purity: f64 = class_freq
         .iter()
@@ -299,26 +339,15 @@ fn gini_impurity(
 
 /// Given an array of labels and the parent and child mask for before and after
 /// a potential split calculate the information gain of making the split.
-fn information_gain(
-    labels: &ArrayBase<impl Data<Elem = u64> + Sync, Ix1>,
-    parent_idxs: &Vec<usize>,
-    row_idxs: &Vec<usize>,
-    n_classes: u64,
-) -> f64 {
-    entropy(&labels, &parent_idxs, n_classes) - entropy(&labels, &row_idxs, n_classes)
+fn information_gain(parent_class_freq: &Vec<u64>, class_freq: &Vec<u64>) -> f64 {
+    entropy(&parent_class_freq) - entropy(&class_freq)
 }
 
 /// Given an array of labels and a mask `row_idxs` calculate the entropy of the
 /// subset.
-fn entropy(
-    labels: &ArrayBase<impl Data<Elem = u64> + Sync, Ix1>,
-    row_idxs: &Vec<usize>,
-    n_classes: u64,
-) -> f64 {
-    let n_samples = row_idxs.len();
+fn entropy(class_freq: &Vec<u64>) -> f64 {
+    let n_samples: u64 = class_freq.iter().sum();
     assert!(n_samples > 0);
-
-    let class_freq = class_frequencies(labels, row_idxs, n_classes);
 
     class_freq
         .iter()
@@ -349,56 +378,38 @@ mod tests {
 
     #[test]
     fn gini_impurity_example() {
-        let n_classes = 3;
-        let labels = Array::from(vec![0, 0, 0, 0, 0, 0, 1, 1]);
-        let all_idxs = (0..8).collect();
+        let class_freq = vec![6, 2, 0];
 
         // Class 0 occurs 75% of the time
         // Class 1 occurs 25% of the time
         // Class 2 occurs 0% of the time
         // Gini impurity is 1 - 0.75*0.75 - 0.25*0.25 - 0*0 = 0.375
-        assert_abs_diff_eq!(
-            gini_impurity(&labels, &all_idxs, n_classes),
-            0.375,
-            epsilon = 1e-5
-        );
+        assert_abs_diff_eq!(gini_impurity(&class_freq), 0.375, epsilon = 1e-5);
     }
 
     #[test]
     fn entropy_example() {
-        let n_classes = 3;
-        let labels = Array::from(vec![0, 0, 0, 0, 0, 0, 1, 1]);
-        let all_idxs = (0..8).collect();
+        let class_freq = vec![6, 2, 0];
 
         // Class 0 occurs 75% of the time
         // Class 1 occurs 25% of the time
         // Class 2 occurs 0% of the time
         // Entropy is -0.75*log2(0.75) - 0.25*log2(0.25) - 0*log2(0) = 0.81127812
-        assert_abs_diff_eq!(
-            entropy(&labels, &all_idxs, n_classes),
-            0.81127,
-            epsilon = 1e-5
-        );
+        assert_abs_diff_eq!(entropy(&class_freq), 0.81127, epsilon = 1e-5);
 
         // If split is perfect then entropy is zero
-        let perfect_labels = Array::from(vec![0, 0, 0, 0, 0, 0, 0, 0]);
-        assert_abs_diff_eq!(
-            entropy(&perfect_labels, &all_idxs, n_classes),
-            0.0,
-            epsilon = 1e-5
-        );
+        let perfect_class_freq = vec![8, 0, 0];
+        assert_abs_diff_eq!(entropy(&perfect_class_freq), 0.0, epsilon = 1e-5);
     }
 
     #[test]
     fn information_gain_example() {
-        let n_classes = 3;
-        let labels = Array::from(vec![0, 0, 0, 0, 0, 1, 1, 2]);
-        let parent_idxs = (0..8).collect();
-        let child_idxs = (0..7).collect();
+        let parent_class_freq = vec![5, 2, 1];
+        let class_freq = vec![5, 2, 0];
 
         // Information gain is just the decrease in entropy from parent to child
         assert_abs_diff_eq!(
-            information_gain(&labels, &parent_idxs, &child_idxs, n_classes),
+            information_gain(&parent_class_freq, &class_freq),
             0.435674,
             epsilon = 1e-5
         );
