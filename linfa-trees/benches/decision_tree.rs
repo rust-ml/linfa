770ea3eb3707ef7ed8eb3c5e5827fb1b0413a350
leaen <mossbanay@gmail.com>
2020-03-27 11:52:01 +1100

    Clean up tests

--- linfa-trees/benches/decision_tree.rs
+++ linfa-trees/benches/decision_tree.rs
@@ -1,6 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use linfa_clustering::generate_blobs;
-use linfa_trees::{DecisionTree, DecisionTreeParams, SplitQuality};
+use linfa_trees::{DecisionTree, DecisionTreeParams};
 use ndarray::{Array, Array2};
 use ndarray_rand::rand::SeedableRng;
 use ndarray_rand::rand_distr::Uniform;
@@ -10,26 +10,28 @@ use std::iter::FromIterator;
 
 fn decision_tree_bench(c: &mut Criterion) {
     let mut rng = Isaac64Rng::seed_from_u64(42);
-    let training_set_sizes = vec![10, 20, 30, 40, 50];
+
+    // Controls how many samples for each class are generated
+    let training_set_sizes = vec![100, 1000, 10000];
 
     let n_classes: u64 = 4;
     let n_features = 4;
 
-    let hyperparams = DecisionTreeParams::new(n_classes as u64)
-        .split_quality(SplitQuality::Gini)
-        .max_depth(Some(50))
-        .min_samples_split(5)
-        .min_samples_leaf(5);
+    // Use the default configuration
+    let hyperparams = DecisionTreeParams::new(n_classes as u64);
 
+    // Benchmark training time 10 times for each training sample size
     let mut group = c.benchmark_group("decision_tree");
+    group.sample_size(10);
+
     for n in training_set_sizes.iter() {
         let centroids = Array2::random_using(
             (n_classes as usize, n_features),
             Uniform::new(-30., 30.),
             &mut rng,
         );
-        let train_x = generate_blobs(*n, &centroids, &mut rng);
 
+        let train_x = generate_blobs(*n, &centroids, &mut rng);
         let train_y = Array::from_iter(
             (0..n_classes)
                 .map(|x| std::iter::repeat(x).take(*n).collect::<Vec<u64>>())
@@ -39,9 +41,10 @@ fn decision_tree_bench(c: &mut Criterion) {
         group.bench_with_input(
             BenchmarkId::from_parameter(n),
             &(train_x, train_y),
-            |b, (x, y)| b.iter(|| black_box(DecisionTree::fit(hyperparams.build(), &x, &y))),
+            |b, (x, y)| b.iter(|| DecisionTree::fit(hyperparams.build(), &x, &y)),
         );
     }
+
     group.finish();
 }
 
