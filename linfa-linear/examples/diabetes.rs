28e0a76c91d17f6efbc9c8df7c2f228b9afdce50
Paul Körbitz <koerbitz@google.com>
2020-05-20 23:11:16 +0200

    Gzip compress diabetes dataset

--- linfa-linear/examples/diabetes.rs
+++ linfa-linear/examples/diabetes.rs
@@ -1,20 +1,21 @@
 use csv::ReaderBuilder;
+use flate2::read::GzDecoder;
+use linfa_linear::LinearRegression;
 use ndarray::Array2;
 use ndarray_csv::Array2Reader;
 use std::error::Error;
 use std::fs::File;
-use linfa_linear::LinearRegression;
 
 fn read_array(path: &str) -> Result<Array2<f64>, Box<dyn Error>> {
-    let file = File::open(path)?;
+    let file = GzDecoder::new(File::open(path)?);
     let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
     let array = reader.deserialize_array2_dynamic()?;
     Ok(array)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let data = read_array("../datasets/diabetes_data.csv")?;
-    let target = read_array("../datasets/diabetes_target.csv")?;
+    let data = read_array("../datasets/diabetes_data.csv.gz")?;
+    let target = read_array("../datasets/diabetes_target.csv.gz")?;
     let target = target.column(0);
 
     let lin_reg = LinearRegression::new().fit_intercept();
