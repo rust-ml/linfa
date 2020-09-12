a07e0dd4c2c03ef767b0da667c36d35bf6988a58
Paul Koerbitz <koerbitz@google.com>
2020-09-12 22:24:18 +0200

    Naive first implementation for coordinate descent
    This implementation relies on the feature matrix to be standardized
    and the target vector to be centered (i.e. best intercept needs to be 0).

--- linfa-elastic/src/lib.rs
+++ linfa-elastic/src/lib.rs
@@ -129,26 +129,70 @@ fn next_beta_j(beta_j: f64, sum_xij_ri: f64, lambda: f64, alpha_lambda: f64) -> 
 }
 
 /// Compute one step of parameter updates
-fn step(x: &Array2<f64>, y: &Array1<f64>, beta: &Array1<f64>, alpha: f64, lambda: f64) -> Array1<f64> {
+fn step(x: &Array2<f64>, y: &Array1<f64>, beta: &Array1<f64>, alpha: f64, lambda: f64) -> (Array1<f64>, f64) {
     let alpha_lambda = alpha * lambda;
     let mut r = y - &x.dot(beta);
     let mut next_beta = beta.clone();
     let n = beta.len();
+    let mut max_change = 0.0;
     for j in 0..n {
         let sum_xij_ri = x.slice(s![.., j]).dot(&r) / n as f64;
         let beta_j = next_beta_j(beta[j], sum_xij_ri, lambda, alpha_lambda);
         if beta_j != beta[j] {
+            let abs_change = (beta_j - beta[j]).abs();
+            if abs_change > max_change {
+                max_change = abs_change;
+            }
             next_beta[j] = beta_j;
             r = y - &x.dot(&next_beta);
         }
     }
-    next_beta
+    (next_beta, max_change)
+}
+
+// FIXME: only handles standardized and centered problems
+fn coordinate_descent(x: &Array2<f64>, y: &Array1<f64>, tol: f64, max_steps: u32, alpha: f64, lambda: f64) -> (Array1<f64>, u32) {
+    let mut beta = Array1::zeros(x.shape()[1]);
+    let mut num_steps = 0u32;
+    let mut max_change = f64::INFINITY;
+    while max_change > tol && num_steps < max_steps {
+        let step_result = step(x, y, &beta, alpha, lambda);
+        beta = step_result.0;
+        max_change = step_result.1;
+        num_steps += 1;
+    }
+    (beta, num_steps)
+}
+
+fn elastic_net_objective(x: &Array2<f64>, y: &Array1<f64>, intercept: f64, beta: &Array1<f64>, alpha: f64, lambda: f64) -> f64 {
+    squared_error(x, y, intercept, beta) + lambda * elastic_net_penalty(beta, alpha)
+}
+
+fn squared_error(x: &Array2<f64>, y: &Array1<f64>, intercept: f64, beta: &Array1<f64>) -> f64 {
+    let mut resid = -x.dot(beta);
+    resid -= intercept;
+    resid += y;
+    let mut result = 0.0;
+    for r in &resid {
+        result += r * r;
+    }
+    result /= 2.0 * y.len() as f64;
+    result
+}
+
+fn elastic_net_penalty(beta: &Array1<f64>, alpha: f64) -> f64 {
+    let mut penalty = 0.0;
+    for beta_j in beta {
+        penalty += (1.0 - alpha) / 2.0 * beta_j * beta_j + alpha * beta_j.abs();
+    }
+    penalty
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{soft_threshold, step};
+    use approx::abs_diff_eq;
     use ndarray::array;
+    use super::{coordinate_descent, elastic_net_objective, elastic_net_penalty, soft_threshold, squared_error, step};
 
     #[test]
     fn soft_threshold_works() {
@@ -164,7 +208,43 @@ mod tests {
         let x = array![[1., 2.], [3., 4.]];
         let y = array![1., 2.];
         let beta = array![0., 0.];
-        let next_beta = step(&x, &y, &beta, 0.5, 1.0);
+        let next_beta = step(&x, &y, &beta, 0.5, 1.0).0;
         assert_ne!(beta, next_beta);
+    }
+
+    #[test]
+    fn elastic_net_penalty_works() {
+        let beta = array![-2.0, 1.0];
+        abs_diff_eq!(elastic_net_penalty(&beta, 0.8), 0.4 + 0.1 + 1.6 + 0.8);
+        abs_diff_eq!(elastic_net_penalty(&beta, 1.0), 3.0);
+        abs_diff_eq!(elastic_net_penalty(&beta, 0.0), 2.5);
+
+        let beta2 = array![0.0, 0.0];
+        abs_diff_eq!(elastic_net_penalty(&beta2, 0.8), 0.0);
+        abs_diff_eq!(elastic_net_penalty(&beta2, 1.0), 0.0);
+        abs_diff_eq!(elastic_net_penalty(&beta2, 0.0), 0.0);
+    }
+
+    #[test]
+    fn squared_error_works() {
+        let x = array![[2.0, 1.0], [-1.0, 2.0]];
+        let y = array![1.0, 1.0];
+        let beta = array![0.0, 1.0];
+        abs_diff_eq!(squared_error(&x, &y, 0.0, &beta), 0.25);
+    }
+
+    #[test]
+    fn coordinate_descent_lowers_objective() {
+        let x = array![[1.0, 0.0], [0.0, 1.0]];
+        let y = array![1.0, -1.0];
+        let beta = array![0.0, 0.0];
+        let intercept = 0.0;
+        let alpha = 0.8;
+        let lambda = 0.001;
+        let objective_start = elastic_net_objective(&x, &y, intercept, &beta, alpha, lambda);
+        let opt_result = coordinate_descent(&x, &y, 1e-4, 3, alpha, lambda);
+        println!("num_steps = {}\nnew_beta = {:?}", opt_result.1, opt_result.0);
+        let objective_end = elastic_net_objective(&x, &y, intercept, &opt_result.0, alpha, lambda);
+        assert!(objective_start > objective_end);
     }
 }
