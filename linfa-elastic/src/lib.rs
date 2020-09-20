5ca9483056cc1c4854775a1526c22b1e4a70cfd5
Paul Koerbitz <koerbitz@google.com>
2020-09-20 17:01:07 +0200

    Use coordinate descent algorithm from sklearn

--- linfa-elastic/src/lib.rs
+++ linfa-elastic/src/lib.rs
@@ -1,18 +1,12 @@
 use approx::{abs_diff_eq, abs_diff_ne};
 use ndarray::{s, Array1, Array2, Axis};
-use ndarray_stats::SummaryStatisticsExt;
-
-pub trait Float {}
 
 /// Linear regression with both L1 and L2 regularization
 ///
 /// Configures and minimizes the following objective function:
 ///             1 / (2 * n_samples) * ||y - Xw||^2_2
-///             + alpha * l1_ratio * ||w||_1
-///             + 0.5 * alpha * (1 - l1_ratio) * ||w||^2_2
-/// where:
-///
-/// alpha = a + b and l1_ration = a / (a + b)
+///             + penalty * l1_ratio * ||w||_1
+///             + 0.5 * penalty * (1 - l1_ratio) * ||w||^2_2
 ///
 pub struct ElasticNet {
     penalty: f64,
@@ -102,36 +96,32 @@ impl ElasticNet {
     pub fn fit(&self, x: &Array2<f64>, y: &Array1<f64>) -> FittedElasticNet {
         let y_mean = y.mean().unwrap();
         let y_centered = y - y_mean;
-        let x_scale = x.map_axis(Axis(0), |col| { 
-            let stddev = col.central_moment(2).unwrap().sqrt(); 
-            // If stddev for a feature is 0.0, replace it with 1.0 to make 
-            // scaling work in all cases (all entries are 0.0 anyway, we're
-            // not changing anything, we're just avoiding to produce NaNs)
-            if abs_diff_eq!(stddev, 0.0) { 1.0 } else { stddev }
-        });
-        let x_scaled = x / &x_scale;
-        eprintln!("y_centered = {}, x_scaled = {}", y_centered, x_scaled);
-        eprintln!(
-            "x_scaled.var() = {}",
-            x_scaled.map_axis(Axis(0), |col| col.central_moment(2).unwrap())
-        );
+        // let x_scale = x.map_axis(Axis(0), |col| {
+        //     let sum_sq = col.fold(0.0, |sum_sq, x| sum_sq + x * x);
+        //     // If stddev for a feature is 0.0, replace it with 1.0 to make
+        //     // scaling work in all cases (all entries are 0.0 anyway, we're
+        //     // not changing anything, we're just avoiding to produce NaNs)
+        //     if abs_diff_eq!(sum_sq, 0.0) {
+        //         1.0
+        //     } else {
+        //         sum_sq
+        //     }
+        // });
+        // let x_scaled = x / &x_scale;
+        // eprintln!("y_centered = {}, x_scaled = {}", y_centered, x_scaled);
+        // eprintln!(
+        //     "x_scaled.var() = {}",
+        //     x_scaled.map_axis(Axis(0), |col| col.central_moment(2).unwrap())
+        // );
         let opt_result = coordinate_descent(
-            &x_scaled,
+            &x,
             &y_centered,
             self.tolerance,
             self.max_iterations,
             self.l1_ratio,
             self.penalty,
         );
-        eprintln!("opt_result.0 = {}", opt_result.0);
-        let parameters = opt_result.0 / &x_scale;
-        eprintln!(
-            "y_mean = {}, x_mean = {}, parameters = {}, x_scale = {}",
-            y_mean,
-            x.mean_axis(Axis(0)).unwrap(),
-            parameters,
-            x_scale
-        );
+        let parameters = opt_result.0; // / &x_scale;
         let intercept = y_mean;
         FittedElasticNet {
             intercept,
@@ -156,142 +146,83 @@ impl FittedElasticNet {
     }
 }
 
-fn soft_threshold(z: f64, gamma: f64) -> f64 {
-    if gamma >= z.abs() {
-        0.0
-    } else if z > 0.0 {
-        z - gamma
-    } else {
-        z + gamma
-    }
-}
-
-/// Computes a single element-wise parameter update
-/// S(\sum_i(x_ij * r_i) + beta_j, lambda * alpha)
-///     / (1 + lambda * (1 - alpha))
-/// where `S` is the soft_threshold function defined above
-fn next_beta_j(beta_j: f64, sum_xij_ri: f64, lambda: f64, alpha_lambda: f64) -> f64 {
-    // quotient could be pre-computed
-    soft_threshold(sum_xij_ri + beta_j, alpha_lambda) / (1.0 + lambda - alpha_lambda)
-}
-
-/// Compute one step of parameter updates
-fn step(
-    x: &Array2<f64>,
-    y: &Array1<f64>,
-    beta: &Array1<f64>,
-    alpha: f64,
-    lambda: f64,
-) -> (Array1<f64>, f64) {
-    let alpha_lambda = alpha * lambda;
-    let mut r = y - &x.dot(beta);
-    let mut next_beta = beta.clone();
-    let n = beta.len();
-    let mut max_change = 0.0;
-    for j in 0..n {
-        let sum_xij_ri = x.slice(s![.., j]).dot(&r) / n as f64;
-        let beta_j = next_beta_j(beta[j], sum_xij_ri, lambda, alpha_lambda);
-        if abs_diff_ne!(beta_j, beta[j]) {
-            let abs_change = (beta_j - beta[j]).abs();
-            if abs_change > max_change {
-                max_change = abs_change;
-            }
-            next_beta[j] = beta_j;
-            r = y - &x.dot(&next_beta);
-        }
-    }
-    (next_beta, max_change)
-}
-
-// FIXME: only handles standardized and centered problems
 fn coordinate_descent(
     x: &Array2<f64>,
     y: &Array1<f64>,
     tol: f64,
     max_steps: u32,
-    alpha: f64,
-    lambda: f64,
+    l1_ratio: f64,
+    penalty: f64,
 ) -> (Array1<f64>, u32) {
-    let mut beta = Array1::zeros(x.shape()[1]);
-    let mut num_steps = 0u32;
-    let mut max_change = f64::INFINITY;
-    while max_change > tol && num_steps < max_steps {
-        let step_result = step(x, y, &beta, alpha, lambda);
-        beta = step_result.0;
-        max_change = step_result.1;
-        num_steps += 1;
-        eprintln!(
-            "step {}: max_change: {}, beta: {}, objective: {}",
-            num_steps,
-            max_change,
-            beta,
-            elastic_net_objective(x, y, 0.0, &beta, alpha, lambda)
-        );
+    let n_samples = x.shape()[0] as f64;
+    let n_features = x.shape()[1];
+    // the parameters of the model
+    let mut w = Array1::<f64>::zeros(n_features);
+    // the residuals: `y - X*w` (since w=0, this is just `y` for now),
+    // the residuals are updated during the algorithm as the parameters change
+    let mut r = y.clone();
+    let mut n_steps = 0u32;
+    let norm_cols_x = x.map_axis(Axis(0), |col| col.fold(0.0, |sum_sq, x| sum_sq + x * x));
+    let mut d_w_max = f64::INFINITY;
+    while n_steps < max_steps && d_w_max > tol {
+        d_w_max = 0.0;
+        for ii in 0..n_features {
+            if abs_diff_eq!(norm_cols_x[ii], 0.0) {
+                continue;
+            }
+            let w_ii = w[ii];
+            if abs_diff_ne!(w_ii, 0.0) {
+                r += &(w_ii * &x.slice(s![.., ii]));
+            }
+            let tmp: f64 = x.slice(s![.., ii]).dot(&r);
+            w[ii] = tmp.signum() * f64::max(tmp.abs() - n_samples * l1_ratio * penalty, 0.0)
+                / (norm_cols_x[ii] + n_samples * (1.0 - l1_ratio) * penalty);
+            if w[ii] != 0.0 {
+                r -= &(w[ii] * &x.slice(s![.., ii]));
+            }
+            let d_w_ii = (w[ii] - w_ii).abs();
+            d_w_max = f64::max(d_w_max, d_w_ii);
+        }
+        n_steps += 1;
     }
-    (beta, num_steps)
-}
-
-fn elastic_net_objective(
-    x: &Array2<f64>,
-    y: &Array1<f64>,
-    intercept: f64,
-    beta: &Array1<f64>,
-    alpha: f64,
-    lambda: f64,
-) -> f64 {
-    eprintln!(
-        "sqE = {}, pen = {}",
-        squared_error(x, y, intercept, beta),
-        lambda * elastic_net_penalty(beta, alpha)
-    );
-    squared_error(x, y, intercept, beta) + lambda * elastic_net_penalty(beta, alpha)
-}
-
-fn squared_error(x: &Array2<f64>, y: &Array1<f64>, intercept: f64, beta: &Array1<f64>) -> f64 {
-    let mut resid = -x.dot(beta);
-    resid -= intercept;
-    resid += y;
-    let mut result = 0.0;
-    for r in &resid {
-        result += r * r;
-    }
-    result /= 2.0 * y.len() as f64;
-    result
-}
-
-fn elastic_net_penalty(beta: &Array1<f64>, alpha: f64) -> f64 {
-    let mut penalty = 0.0;
-    for beta_j in beta {
-        penalty += (1.0 - alpha) / 2.0 * beta_j * beta_j + alpha * beta_j.abs();
-    }
-    penalty
+    (w, n_steps)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{
-        coordinate_descent, elastic_net_objective, elastic_net_penalty, soft_threshold,
-        squared_error, step, ElasticNet,
-    };
-    use approx::{assert_abs_diff_eq, assert_abs_diff_ne};
-    use ndarray::array;
+    use super::{coordinate_descent, ElasticNet};
+    use approx::assert_abs_diff_eq;
+    use ndarray::{array, Array1, Array2};
 
-    #[test]
-    fn soft_threshold_works() {
-        assert_abs_diff_eq!(soft_threshold(1.0, 1.0), 0.0);
-        assert_abs_diff_eq!(soft_threshold(-1.0, 1.0), 0.0);
-        assert_abs_diff_eq!(soft_threshold(0.0, 1.0), 0.0);
-        assert_abs_diff_eq!(soft_threshold(2.0, 1.0), 1.0);
-        assert_abs_diff_eq!(soft_threshold(-2.0, 1.0), -1.0);
+    fn elastic_net_objective(
+        x: &Array2<f64>,
+        y: &Array1<f64>,
+        intercept: f64,
+        beta: &Array1<f64>,
+        alpha: f64,
+        lambda: f64,
+    ) -> f64 {
+        squared_error(x, y, intercept, beta) + lambda * elastic_net_penalty(beta, alpha)
     }
 
-    #[test]
-    fn step_does_something() {
-        let x = array![[1., 2.], [3., 4.]];
-        let y = array![1., 2.];
-        let beta = array![0., 0.];
-        let next_beta = step(&x, &y, &beta, 0.5, 1.0).0;
-        assert_abs_diff_ne!(beta, next_beta);
+    fn squared_error(x: &Array2<f64>, y: &Array1<f64>, intercept: f64, beta: &Array1<f64>) -> f64 {
+        let mut resid = -x.dot(beta);
+        resid -= intercept;
+        resid += y;
+        let mut result = 0.0;
+        for r in &resid {
+            result += r * r;
+        }
+        result /= 2.0 * y.len() as f64;
+        result
+    }
+
+    fn elastic_net_penalty(beta: &Array1<f64>, alpha: f64) -> f64 {
+        let mut penalty = 0.0;
+        for beta_j in beta {
+            penalty += (1.0 - alpha) / 2.0 * beta_j * beta_j + alpha * beta_j.abs();
+        }
+        penalty
     }
 
     #[test]
@@ -329,25 +260,8 @@ mod tests {
         let lambda = 0.001;
         let objective_start = elastic_net_objective(&x, &y, intercept, &beta, alpha, lambda);
         let opt_result = coordinate_descent(&x, &y, 1e-4, 3, alpha, lambda);
-        println!(
-            "num_steps = {}\nnew_beta = {:?}",
-            opt_result.1, opt_result.0
-        );
         let objective_end = elastic_net_objective(&x, &y, intercept, &opt_result.0, alpha, lambda);
         assert!(objective_start > objective_end);
-    }
-
-    #[test]
-    fn simple_elastic_net_works() {
-        let x = array![[1.0, 0.0], [0.0, 1.0]];
-        let y = array![3.0, 2.0];
-        let model = ElasticNet::new()
-            .max_iterations(10_000)
-            .penalty(0.01)
-            .l1_ratio(0.5)
-            .fit(&x, &y);
-        assert_abs_diff_eq!(model.intercept(), 2.5);
-        assert_abs_diff_eq!(model.parameters(), &array![0.493, -0.493], epsilon = 0.001);
     }
 
     #[test]
@@ -357,5 +271,72 @@ mod tests {
         let model = ElasticNet::new().l1_ratio(1.0).penalty(0.1).fit(&x, &y);
         assert_abs_diff_eq!(model.intercept(), 0.);
         assert_abs_diff_eq!(model.parameters(), &array![0.]);
+    }
+
+    #[test]
+    fn lasso_toy_example_works() {
+        // Test Lasso on a toy example for various values of alpha.
+        // When validating this against glmnet notice that glmnet divides it
+        // against n_samples.
+        let x = array![[-1.0], [0.0], [1.0]];
+        let y = array![-1.0, 0.0, 1.0];
+        let model = ElasticNet::new().l1_ratio(1.0).penalty(1e-8).fit(&x, &y);
+        assert_abs_diff_eq!(model.intercept(), 0.0);
+        assert_abs_diff_eq!(model.parameters(), &array![1.0], epsilon = 1e-6);
+        // let t = array![[2.0], [3.0], [4.0]];
+        // assert_abs_diff_eq!(model.predict(&t), &t);
+        // Still have to port this from python:
+        // # assert_almost_equal(clf.dual_gap_, 0)
+        //
+        let model = ElasticNet::new().l1_ratio(1.0).penalty(0.1).fit(&x, &y);
+        assert_abs_diff_eq!(model.intercept(), 0.0);
+        assert_abs_diff_eq!(model.parameters(), &array![0.85], epsilon = 1e-6);
+        // Still to implement
+        // pred = clf.predict(T)
+        // assert_array_almost_equal(pred, [1.7, 2.55, 3.4])
+        // assert_almost_equal(clf.dual_gap_, 0)
+
+        let model = ElasticNet::new().l1_ratio(1.0).penalty(0.5).fit(&x, &y);
+        assert_abs_diff_eq!(model.intercept(), 0.0);
+        assert_abs_diff_eq!(model.parameters(), &array![0.25], epsilon = 1e-6);
+        // Still to implement
+        // pred = clf.predict(T)
+        // assert_array_almost_equal(pred, [0.5, 0.75, 1.])
+        // assert_almost_equal(clf.dual_gap_, 0)
+
+        let model = ElasticNet::new().l1_ratio(1.0).penalty(1.0).fit(&x, &y);
+        assert_abs_diff_eq!(model.intercept(), 0.0);
+        assert_abs_diff_eq!(model.parameters(), &array![0.0], epsilon = 1e-6);
+        // Still to implement
+        // pred = clf.predict(T)
+        // assert_array_almost_equal(pred, [0, 0, 0])
+        // assert_almost_equal(clf.dual_gap_, 0)
+    }
+
+    #[test]
+    fn elastic_net_toy_example_works() {
+        let x = array![[-1.0], [0.0], [1.0]];
+        let y = array![-1.0, 0.0, 1.0];
+        let model = ElasticNet::new().l1_ratio(0.3).penalty(0.5).fit(&x, &y);
+        assert_abs_diff_eq!(model.intercept(), 0.0);
+        assert_abs_diff_eq!(model.parameters(), &array![0.50819], epsilon = 1e-3);
+        // assert_array_almost_equal(pred, [1.0163, 1.5245, 2.0327], decimal=3)
+        // assert_almost_equal(clf.dual_gap_, 0)
+
+        let model = ElasticNet::new().l1_ratio(0.5).penalty(0.5).fit(&x, &y);
+        assert_abs_diff_eq!(model.intercept(), 0.0);
+        assert_abs_diff_eq!(model.parameters(), &array![0.45454], epsilon = 1e-3);
+        // pred = clf.predict(T)
+        // assert_array_almost_equal(pred, [0.9090, 1.3636, 1.8181], 3)
+        // assert_almost_equal(clf.dual_gap_, 0)
+    }
+
+    #[test]
+    fn elastic_net_2d_toy_example_works() {
+        let x = array![[1.0, 0.0], [0.0, 1.0]];
+        let y = array![3.0, 2.0];
+        let model = ElasticNet::new().penalty(0.0).fit(&x, &y);
+        assert_abs_diff_eq!(model.intercept(), 2.5);
+        assert_abs_diff_eq!(model.parameters(), &array![0.5, -0.5], epsilon = 0.001);
     }
 }
