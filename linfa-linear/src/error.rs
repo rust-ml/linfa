2b6ab1376573ea485d6439bb7330b1badcccc90d
VasanthakumarV <vasanth260m12@gmail.com>
2020-09-26 23:17:48 +0530

    Add error conversion from argmin to crate error

--- linfa-linear/src/error.rs
+++ linfa-linear/src/error.rs
@@ -6,14 +6,22 @@ pub type Result<T> = std::result::Result<T, LinearError>;
 #[derive(Debug)]
 pub enum LinearError {
     InvalidValue(String),
+    ArgminError(argmin::core::Error),
 }
 
 impl Display for LinearError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::InvalidValue(message) => write!(f, "Invalid value encountered: {}", message),
+            Self::ArgminError(error) => write!(f, "Argmin Error: {}", error),
         }
     }
 }
 
 impl Error for LinearError {}
+
+impl From<argmin::core::Error> for LinearError {
+    fn from(error: argmin::core::Error) -> LinearError {
+        LinearError::ArgminError(error)
+    }
+}
