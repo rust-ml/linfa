7e757a854b867edb544ff60ac31d593d88411416
VasanthakumarV <vasanth260m12@gmail.com>
2020-09-26 13:47:22 +0530

    Add self to link trait impl

--- linfa-linear/src/glm/link.rs
+++ linfa-linear/src/glm/link.rs
@@ -2,29 +2,29 @@ use ndarray::Array1;
 
 use crate::Float;
 
-trait Link<A> {
-    fn link(ypred: &Array1<A>) -> Array1<A>;
-    fn link_derivative(ypred: &Array1<A>) -> Array1<A>;
-    fn inverse(ypred: &Array1<A>) -> Array1<A>;
-    fn inverse_derivative(ypred: &Array1<A>) -> Array1<A>;
+pub trait Link<A> {
+    fn link(&self, ypred: &Array1<A>) -> Array1<A>;
+    fn link_derivative(&self, ypred: &Array1<A>) -> Array1<A>;
+    fn inverse(&self, ypred: &Array1<A>) -> Array1<A>;
+    fn inverse_derivative(&self, ypred: &Array1<A>) -> Array1<A>;
 }
 
-struct IdentityLink;
+pub struct IdentityLink;
 
 impl<A: Float> Link<A> for IdentityLink {
-    fn link(ypred: &Array1<A>) -> Array1<A> {
+    fn link(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.clone()
     }
 
-    fn link_derivative(ypred: &Array1<A>) -> Array1<A> {
+    fn link_derivative(&self, ypred: &Array1<A>) -> Array1<A> {
         Array1::ones(ypred.shape()[0])
     }
 
-    fn inverse(ypred: &Array1<A>) -> Array1<A> {
+    fn inverse(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.clone()
     }
 
-    fn inverse_derivative(ypred: &Array1<A>) -> Array1<A> {
+    fn inverse_derivative(&self, ypred: &Array1<A>) -> Array1<A> {
         Array1::ones(ypred.shape()[0])
     }
 }
@@ -32,11 +32,11 @@ impl<A: Float> Link<A> for IdentityLink {
 struct LogLink;
 
 impl<A: Float> Link<A> for LogLink {
-    fn link(ypred: &Array1<A>) -> Array1<A> {
+    fn link(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.mapv(|x| x.ln())
     }
 
-    fn link_derivative(ypred: &Array1<A>) -> Array1<A> {
+    fn link_derivative(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.mapv(|x| {
             let lower_bound = A::from(1e-7).unwrap();
             if x < lower_bound {
@@ -46,11 +46,11 @@ impl<A: Float> Link<A> for LogLink {
         })
     }
 
-    fn inverse(ypred: &Array1<A>) -> Array1<A> {
+    fn inverse(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.mapv(|x| x.exp())
     }
 
-    fn inverse_derivative(ypred: &Array1<A>) -> Array1<A> {
+    fn inverse_derivative(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.mapv(|x| x.exp())
     }
 }
@@ -58,19 +58,19 @@ impl<A: Float> Link<A> for LogLink {
 struct LogitLink;
 
 impl<A: Float> Link<A> for LogitLink {
-    fn link(ypred: &Array1<A>) -> Array1<A> {
+    fn link(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.mapv(|x| (x / (A::from(1.).unwrap() - x)).ln())
     }
 
-    fn link_derivative(ypred: &Array1<A>) -> Array1<A> {
+    fn link_derivative(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.mapv(|x| A::from(1.).unwrap() / (x * (A::from(1.).unwrap() - x)))
     }
 
-    fn inverse(ypred: &Array1<A>) -> Array1<A> {
+    fn inverse(&self, ypred: &Array1<A>) -> Array1<A> {
         ypred.mapv(|x| A::from(1.).unwrap() / (A::from(1.).unwrap() + x.neg().exp()))
     }
 
-    fn inverse_derivative(ypred: &Array1<A>) -> Array1<A> {
+    fn inverse_derivative(&self, ypred: &Array1<A>) -> Array1<A> {
         let expit = ypred.mapv(|x| A::from(1.).unwrap() / (A::from(1.).unwrap() + x.neg().exp()));
 
         let one_minus_expit = expit.mapv(|x| A::from(1.).unwrap() - x);
@@ -85,13 +85,25 @@ mod tests {
     use approx::assert_abs_diff_eq;
     use ndarray::array;
 
+    //#[test]
+    //fn test_sample() {
+    //let link = IdentityLink;
+    //let input = vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]];
+    //let expected = vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]];
+    //for (expected, input) in expected.iter().zip(input.iter()) {
+    //let output = link.link(input);
+    //assert_abs_diff_eq!(output, expected, epsilon = 1e-6);
+    //}
+    //}
+
     macro_rules! test_links {
-        ($($func:ident: {input: $input:expr, expected: $expected:expr, link: $link:expr}),*) => {
+        ($($func:ident: {input: $input:expr, expected: $expected:expr, link: $link:ident, method: $method:ident,},)*) => {
             $(
                 #[test]
                 fn $func() {
+                    let link = $link;
                     for (expected, input) in $expected.iter().zip($input.iter()) {
-                        let output = $link(input);
+                        let output = link.$method(input);
                         assert_abs_diff_eq!(output, expected, epsilon = 1e-6);
                     }
                 }
@@ -103,23 +115,27 @@ mod tests {
         test_identity_link: {
             input: vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]],
             expected: vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]],
-            link: IdentityLink::link
+            link: IdentityLink,
+            method: link,
         },
         test_identity_link_derivative: {
             input: vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]],
             expected: vec![array![1., 1., 1., 1.], array![1., 1., 1., 1.]],
-            link: IdentityLink::link_derivative
+            link: IdentityLink,
+            method: link_derivative,
         },
         test_identity_inverse: {
             input: vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]],
             expected: vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]],
-            link: IdentityLink::inverse
+            link: IdentityLink,
+            method: inverse,
         },
         test_identity_inverse_derivative: {
             input: vec![array![1., 1., 1., 1.], array![1.348, 2.879, 4.545, 3.232]],
             expected: vec![array![1., 1., 1., 1.], array![1., 1., 1., 1.]],
-            link: IdentityLink::inverse_derivative
-        }
+            link: IdentityLink,
+            method: inverse_derivative,
+        },
     ];
 
     test_links! [
@@ -132,7 +148,8 @@ mod tests {
                 array![0.32353173, 0.28442678, 0.27763174, 0.27914574],
                 array![3.82085464, 3.76120012, -15.42494847, -1.07294454],
             ],
-            link: LogLink::link
+            link: LogLink,
+            method: link,
         },
         test_log_link_derivative: {
             input: vec![
@@ -148,7 +165,8 @@ mod tests {
                     2.92397661e+00
                 ],
             ],
-            link: LogLink::link_derivative
+            link: LogLink,
+            method: link_derivative,
         },
         test_log_inverse: {
             input: vec![
@@ -159,7 +177,8 @@ mod tests {
                 array![3.98285939, 3.77726423, 3.74342138, 3.75091571],
                 array![6.646452e+19, 4.72783947e+18, 1.00000020e+00, 1.40776030e+00],
             ],
-            link: LogLink::inverse
+            link: LogLink,
+            method: inverse,
         },
         test_log_inverse_derivative: {
             input: vec![
@@ -170,8 +189,9 @@ mod tests {
                 array![3.98285939, 3.77726423, 3.74342138, 3.75091571],
                 array![6.646452e+19, 4.72783947e+18, 1.00000020e+00, 1.40776030e+00],
             ],
-            link: LogLink::inverse_derivative
-        }
+            link: LogLink,
+            method: inverse_derivative,
+        },
     ];
 
     test_links! [
@@ -183,7 +203,8 @@ mod tests {
                 array![2.6498217, -0.74001895, 4.49879906, -0.3557036 ],
                 array![-3.07856828, -3.74899244,  6.90675478, -1.95508453],
             ],
-            link: LogitLink::link
+            link: LogitLink,
+            method: link,
         },
         test_logit_link_derivative: {
             input: vec![array![0.934, 0.323, 0.989, 0.412], array![0.044, 0.023, 0.999, 0.124]],
@@ -191,7 +212,8 @@ mod tests {
                 array![16.22217896, 4.57308011, 91.92021325, 4.12786474],
                 array![23.77329783, 44.50180232, 1001.001001, 9.20606864],
             ],
-            link: LogitLink::link_derivative
+            link: LogitLink,
+            method: link_derivative,
         },
         test_logit_inverse: {
             input: vec![array![0.934, 0.323, 0.989, 0.412], array![0.044, 0.023, 0.999, 0.124]],
@@ -199,7 +221,8 @@ mod tests {
                 array![0.71788609, 0.5800552, 0.72889036, 0.60156734],
                 array![0.51099823, 0.50574975, 0.73086192, 0.53096034],
             ],
-            link: LogitLink::inverse
+            link: LogitLink,
+            method: inverse,
         },
         test_logit_inverse_derivative: {
             input: vec![array![0.934, 0.323, 0.989, 0.412], array![0.044, 0.023, 0.999, 0.124]],
@@ -207,7 +230,8 @@ mod tests {
                 array![0.20252565, 0.24359116, 0.1976092, 0.23968407],
                 array![0.24987904, 0.24996694, 0.19670277, 0.24904146],
             ],
-            link: LogitLink::inverse_derivative
-        }
+            link: LogitLink,
+            method: inverse_derivative,
+        },
     ];
 }
