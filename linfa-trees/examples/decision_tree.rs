405f2f7ee689bd96497501408c5a941b513cf7a4
Lorenz Schmidt <bytesnake@mailbox.org>
2020-11-29 17:37:21 +0100

    Release 0.2.1
     * remove features for testing
     * fill up some missing information in Cargo.toml
     * make ready to publish to crates.io

--- linfa-trees/examples/decision_tree.rs
+++ linfa-trees/examples/decision_tree.rs
@@ -1,9 +1,52 @@
-use linfa_clustering::generate_blobs;
 use linfa_trees::{DecisionTree, DecisionTreeParams, SplitQuality};
-use ndarray::{array, Array, ArrayBase, Data, Ix1};
+use ndarray::{array, Array, ArrayBase, Data, Ix1, Ix2, Array2, s};
+use ndarray_rand::rand::Rng;
 use ndarray_rand::rand::SeedableRng;
+use ndarray_rand::rand_distr::StandardNormal;
+use ndarray_rand::RandomExt;
 use rand_isaac::Isaac64Rng;
 use std::iter::FromIterator;
+
+/// Given an input matrix `blob_centroids`, with shape `(n_blobs, n_features)`,
+/// generate `blob_size` data points (a "blob") around each of the blob centroids.
+///
+/// More specifically, each blob is formed by `blob_size` points sampled from a normal
+/// distribution centered in the blob centroid with unit variance.
+///
+/// `generate_blobs` can be used to quickly assemble a synthetic dataset to test or
+/// benchmark various clustering algorithms on a best-case scenario input.
+pub fn generate_blobs(
+    blob_size: usize,
+    blob_centroids: &ArrayBase<impl Data<Elem = f64>, Ix2>,
+    rng: &mut impl Rng,
+) -> Array2<f64> {
+    let (n_centroids, n_features) = blob_centroids.dim();
+    let mut blobs: Array2<f64> = Array2::zeros((n_centroids * blob_size, n_features));
+
+    for (blob_index, blob_centroid) in blob_centroids.genrows().into_iter().enumerate() {
+        let blob = generate_blob(blob_size, &blob_centroid, rng);
+
+        let indexes = s![blob_index * blob_size..(blob_index + 1) * blob_size, ..];
+        blobs.slice_mut(indexes).assign(&blob);
+    }
+    blobs
+}
+
+/// Generate `blob_size` data points (a "blob") around `blob_centroid`.
+///
+/// More specifically, the blob is formed by `blob_size` points sampled from a normal
+/// distribution centered in `blob_centroid` with unit variance.
+///
+/// `generate_blob` can be used to quickly assemble a synthetic stereotypical cluster.
+pub fn generate_blob(
+    blob_size: usize,
+    blob_centroid: &ArrayBase<impl Data<Elem = f64>, Ix1>,
+    rng: &mut impl Rng,
+) -> Array2<f64> {
+    let shape = (blob_size, blob_centroid.len());
+    let origin_blob: Array2<f64> = Array::random_using(shape, StandardNormal, rng);
+    origin_blob + blob_centroid
+}
 
 fn accuracy(
     labels: &ArrayBase<impl Data<Elem = u64>, Ix1>,
