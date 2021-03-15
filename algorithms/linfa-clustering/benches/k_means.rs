0312e1fd1b64dcaf4344a99ddb182131cad6aa97
YuhanLiin <linyuhan0315@hotmail.com>
2021-03-14 22:28:52 -0400

    Update k means benchmark to use BenchmarkGroups

--- algorithms/linfa-clustering/benches/k_means.rs
+++ algorithms/linfa-clustering/benches/k_means.rs
@@ -1,5 +1,5 @@
 use criterion::{
-    black_box, criterion_group, criterion_main, AxisScale, Criterion, ParameterizedBenchmark,
+    black_box, criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion,
     PlotConfiguration,
 };
 use linfa::traits::Fit;
@@ -15,27 +15,32 @@ fn k_means_bench(c: &mut Criterion) {
     let mut rng = Isaac64Rng::seed_from_u64(40);
     let cluster_sizes = vec![10, 100, 1000, 10000];
 
-    let benchmark = ParameterizedBenchmark::new(
-        "naive_k_means",
-        move |bencher, &cluster_size| {
-            let n_clusters = 4;
-            let n_features = 3;
-            let centroids =
-                Array2::random_using((n_clusters, n_features), Uniform::new(-30., 30.), &mut rng);
-            let dataset = DatasetBase::from(generate_blobs(cluster_size, &centroids, &mut rng));
-            bencher.iter(|| {
-                black_box(
-                    KMeans::params_with_rng(n_clusters, rng.clone())
-                        .max_n_iterations(1000)
-                        .tolerance(1e-3)
-                        .fit(&dataset),
-                )
-            });
-        },
-        cluster_sizes,
-    )
-    .plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
-    c.bench("naive_k_means", benchmark);
+    let mut benchmark = c.benchmark_group("naive_k_means");
+    benchmark.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+    for cluster_size in cluster_sizes {
+        let rng = &mut rng;
+        benchmark.bench_with_input(
+            BenchmarkId::new("naive_k_means", cluster_size),
+            &cluster_size,
+            move |bencher, &cluster_size| {
+                let n_clusters = 4;
+                let n_features = 3;
+                let centroids =
+                    Array2::random_using((n_clusters, n_features), Uniform::new(-30., 30.), rng);
+                let dataset = DatasetBase::from(generate_blobs(cluster_size, &centroids, rng));
+                bencher.iter(|| {
+                    black_box(
+                        KMeans::params_with_rng(n_clusters, rng.clone())
+                            .max_n_iterations(1000)
+                            .tolerance(1e-3)
+                            .fit(&dataset),
+                    )
+                });
+            },
+        );
+    }
+
+    benchmark.finish();
 }
 
 criterion_group! {
