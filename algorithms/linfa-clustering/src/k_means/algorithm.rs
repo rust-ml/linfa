5267f11095feace7422256b34a738b699fdde699
Yuhan Lin <yuhanliin+github@protonmail.com>
2021-04-08 06:34:05 +0000

    Add additional bounds to Float trait (#118)
    * Add bounds to Float trait, allowing trait bounds in dependant crates to be simplified
    
    * Pin nightly toolchain
    
    * Retry pinning toolchain
    
    * Fix CI
    
    * Move nightly version back to 2021-3-24
    
    * Fix tarpaulin nightly invocation

--- algorithms/linfa-clustering/src/k_means/algorithm.rs
+++ algorithms/linfa-clustering/src/k_means/algorithm.rs
@@ -3,10 +3,9 @@ use crate::k_means::hyperparameters::{KMeansHyperParams, KMeansHyperParamsBuilde
 use linfa::{traits::*, DatasetBase, Float};
 use ndarray::{Array1, Array2, ArrayBase, Axis, Data, DataMut, Ix1, Ix2, Zip};
 use ndarray_rand::rand::Rng;
-use ndarray_rand::rand::{distributions::uniform::SampleUniform, SeedableRng};
+use ndarray_rand::rand::SeedableRng;
 use ndarray_stats::DeviationExt;
 use rand_isaac::Isaac64Rng;
-use std::ops::AddAssign;
 
 #[cfg(feature = "serde")]
 use serde_crate::{Deserialize, Serialize};
@@ -129,7 +128,7 @@ pub struct KMeans<F: Float> {
     cost: F,
 }
 
-impl<F: Float + SampleUniform + for<'a> AddAssign<&'a F>> KMeans<F> {
+impl<F: Float> KMeans<F> {
     pub fn params(nclusters: usize) -> KMeansHyperParamsBuilder<F, Isaac64Rng> {
         KMeansHyperParams::new(nclusters)
     }
@@ -158,13 +157,8 @@ impl<F: Float + SampleUniform + for<'a> AddAssign<&'a F>> KMeans<F> {
     }
 }
 
-impl<
-        'a,
-        F: Float + SampleUniform + for<'b> AddAssign<&'b F>,
-        R: Rng + Clone + SeedableRng,
-        D: Data<Elem = F>,
-        T,
-    > Fit<'a, ArrayBase<D, Ix2>, T> for KMeansHyperParams<F, R>
+impl<'a, F: Float, R: Rng + Clone + SeedableRng, D: Data<Elem = F>, T> Fit<'a, ArrayBase<D, Ix2>, T>
+    for KMeansHyperParams<F, R>
 {
     type Object = Result<KMeans<F>>;
 
@@ -235,13 +229,8 @@ impl<
     }
 }
 
-impl<
-        'a,
-        F: Float + SampleUniform + for<'b> AddAssign<&'b F>,
-        R: Rng + SeedableRng + Clone,
-        D: Data<Elem = F>,
-        T,
-    > Fit<'a, ArrayBase<D, Ix2>, T> for KMeansHyperParamsBuilder<F, R>
+impl<'a, F: Float, R: Rng + SeedableRng + Clone, D: Data<Elem = F>, T> Fit<'a, ArrayBase<D, Ix2>, T>
+    for KMeansHyperParamsBuilder<F, R>
 {
     type Object = Result<KMeans<F>>;
 
