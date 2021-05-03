918b62dabf9bc42ad3b0d2930286a06c2c97729e
YuhanLiin <yuhanliin+github@protonmail.com>
2021-05-03 15:49:43 -0400

    Add within_range benchmarks

--- algorithms/linfa-nn/benches/nn.rs
+++ algorithms/linfa-nn/benches/nn.rs
@@ -38,9 +38,7 @@ fn k_nearest_bench(c: &mut Criterion) {
     let mut rng = Isaac64Rng::seed_from_u64(40);
     let mut benchmark = c.benchmark_group("k_nearest");
     let n_features = 3;
-    let n_points = 50000;
     let distr = Uniform::new(-500., 500.);
-    let points = Array2::random_using((n_points, n_features), distr, &mut rng);
 
     let algorithms: &[(Box<dyn NearestNeighbourBuilder<f64>>, _)] = &[
         (Box::new(LinearSearchBuilder::new()), "linear search"),
@@ -48,17 +46,49 @@ fn k_nearest_bench(c: &mut Criterion) {
         (Box::new(BallTreeBuilder::new()), "balltree"),
     ];
 
-    for k in &[10, 50, 100] {
+    for &(n_points, k) in &[(10000, 10), (50000, 100), (50000, 1000)] {
         let pt = Array1::random_using(n_features, distr, &mut rng);
+        let points = Array2::random_using((n_points, n_features), distr, &mut rng);
 
         for (alg, name) in algorithms {
             let nn = alg.from_batch(&points, CommonDistance::SqL2Dist).unwrap();
             benchmark.bench_with_input(
-                BenchmarkId::new(*name, format!("{}", k)),
+                BenchmarkId::new(*name, format!("{}-{}", n_points, k)),
                 &k,
-                |bencher, &&k| {
+                |bencher, &k| {
                     bencher.iter(|| {
                         nn.k_nearest(pt.view(), k).unwrap();
+                    });
+                },
+            );
+        }
+    }
+}
+
+fn within_range_bench(c: &mut Criterion) {
+    let mut rng = Isaac64Rng::seed_from_u64(40);
+    let mut benchmark = c.benchmark_group("within_range");
+    let n_features = 3;
+    let distr = Uniform::new(-50., 50.);
+
+    let algorithms: &[(Box<dyn NearestNeighbourBuilder<f64>>, _)] = &[
+        (Box::new(LinearSearchBuilder::new()), "linear search"),
+        (Box::new(KdTreeBuilder::new()), "kdtree"),
+        (Box::new(BallTreeBuilder::new()), "balltree"),
+    ];
+
+    for &(n_points, range) in &[(50000, 50.0), (50000, 100.0)] {
+        let pt = Array1::random_using(n_features, distr, &mut rng);
+        let points = Array2::random_using((n_points, n_features), distr, &mut rng);
+
+        for (alg, name) in algorithms {
+            let nn = alg.from_batch(&points, CommonDistance::SqL2Dist).unwrap();
+            benchmark.bench_with_input(
+                BenchmarkId::new(*name, format!("{}-{}", n_points, range)),
+                &range,
+                |bencher, &range| {
+                    bencher.iter(|| {
+                        nn.within_range(pt.view(), range).unwrap();
                     });
                 },
             );
@@ -69,6 +99,6 @@ fn k_nearest_bench(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = nn_build_bench, k_nearest_bench
+    targets = nn_build_bench, k_nearest_bench, within_range_bench
 }
 criterion_main!(benches);
