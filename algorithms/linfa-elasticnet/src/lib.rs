e23d876930ca91f223bee38caeba667acea624e8
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-19 20:28:54 +0100

    added MultiTaskEnet struct

--- algorithms/linfa-elasticnet/src/lib.rs
+++ algorithms/linfa-elasticnet/src/lib.rs
@@ -1,7 +1,7 @@
 #![doc = include_str!("../README.md")]
 
 use linfa::Float;
-use ndarray::Array1;
+use ndarray::{Array1, Array2};
 
 #[cfg(feature = "serde")]
 use serde_crate::{Deserialize, Serialize};
@@ -61,6 +61,50 @@ impl<F: Float> ElasticNet<F> {
     }
 
     /// Create a LASSO only model
+    pub fn lasso() -> ElasticNetParams<F> {
+        ElasticNetParams::new().l1_ratio(F::one())
+    }
+}
+
+/// MultiTask Elastic Net model
+///
+/// This struct contains the parameters of a fitted multi-task elastic net model. This includes the
+/// coefficients (a 2-dimensional array), (optionally) intercept (a 1-dimensional array), duality gaps
+/// and the number of steps needed in the computation.
+///
+/// ## Model implementation
+///
+/// The block coordinate descent is widely used to solve generalized linear models optimization problems,
+/// like Group Lasso, MultiTask Ridge or MultiTask Lasso. It cycles through a group of parameters and update
+/// the groups separately, holding all the others fixed. The optimization routine stops when a criterion is
+/// satisfied (dual sub-optimality gap or change in coefficients).
+pub struct MultiTaskElasticNet<F> {
+    hyperplane: Array2<F>,
+    intercept: Array1<F>,
+    duality_gap: F,
+    n_steps: u32,
+    variance: Result<Array2<F>>,
+}
+
+impl<F: Float> MultiTaskElasticNet<F> {
+    /// Create a default parameter set for construction of MultiTaskElasticNet model
+    ///
+    /// By default, an intercept will be fitted. To disable fitting an
+    /// intercept, call `.with_intercept(false)` before calling `.fit()`.
+    ///
+    /// To additionally normalize the feature matrix before fitting, call
+    /// `fit_intercept_and_normalize()` before calling `fit()`. The feature
+    /// matrix will not be normalized by default.
+    pub fn params() -> ElasticNetParams<F> {
+        ElasticNetParams::new()
+    }
+
+    /// Create a multi-task ridge only model
+    pub fn ridge() -> ElasticNetParams<F> {
+        ElasticNetParams::new().l1_ratio(F::zero())
+    }
+
+    /// Create a multi-task Lasso only model
     pub fn lasso() -> ElasticNetParams<F> {
         ElasticNetParams::new().l1_ratio(F::one())
     }
