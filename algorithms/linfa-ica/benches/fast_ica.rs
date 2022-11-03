f803ae2733e064142b6922b2a16d8e59f13ab74b
Femi <47154698+oojo12@users.noreply.github.com>
2022-11-02 23:08:17 -0400

    Create fast_ica.rs

---
+++ algorithms/linfa-ica/benches/fast_ica.rs
@@ -0,0 +1,65 @@
+use linfa::{
+    dataset::DatasetBase,
+    traits::Fit,
+};
+use linfa_ica::fast_ica::{FastIca, GFunc};
+use ndarray::{array, concatenate};
+use ndarray::{Array, Array2, Axis};
+use rand_xoshiro::Xoshiro256Plus;
+use ndarray_rand::{rand::SeedableRng, rand_distr::Uniform, RandomExt};
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+
+fn perform_ica(size: usize) -> () {
+
+    let sources_mixed = create_data(size);
+
+    let ica = FastIca::params().gfunc(GFunc::Logcosh(1.0));
+
+    let ica = ica.fit(&DatasetBase::from(sources_mixed.view()));
+}
+
+fn create_data(nsamples: usize) -> Array2<f64> {
+    // Creating a sine wave signal
+    let source1 = Array::linspace(0., 8., nsamples).mapv(|x| (2f64 * x).sin());
+
+    // Creating a sawtooth signal
+    let source2 = Array::linspace(0., 8., nsamples).mapv(|x| {
+        let tmp = (4f64 * x).sin();
+        if tmp > 0. {
+            return 1.;
+        }
+        -1.
+    });
+
+    // Column concatenating both the signals
+    let mut sources_original = concatenate![
+        Axis(1),
+        source1.insert_axis(Axis(1)),
+        source2.insert_axis(Axis(1))
+    ];
+
+    // Adding noise to the signals
+    let mut rng = Xoshiro256Plus::seed_from_u64(42);
+    sources_original +=
+        &Array::random_using((nsamples, 2), Uniform::new(0.0, 1.0), &mut rng).mapv(|x| x * 0.2);
+
+    // Mixing the two signals
+    let mixing = array![[1., 1.], [0.5, 2.]];
+    let sources_mixed = sources_original.dot(&mixing.t());
+
+    sources_mixed
+}
+
+fn bench(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("fast_ica_bench");
+    for size in [1_000, 10_000, 100_000].iter() {
+        group.bench_with_input(BenchmarkId::new("fast-ica-{}", size), size, |b, &size| {
+            b.iter(|| perform_ica(size));
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
