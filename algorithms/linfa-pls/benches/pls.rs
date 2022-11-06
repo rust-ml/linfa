f28aab8eb6dde63c7a6f29078d50cc0cc81b4402
Femi Oho <47154698+oojo12@users.noreply.github.com>
2022-11-06 17:11:10 -0500

    omit dataset creation from bench timing

--- algorithms/linfa-pls/benches/pls.rs
+++ algorithms/linfa-pls/benches/pls.rs
@@ -2,31 +2,39 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use linfa::traits::Fit;
 use linfa::Dataset;
 use linfa_datasets::generate::make_dataset;
+use linfa_pls::Algorithm;
 use linfa_pls::PlsRegression;
 use statrs::distribution::{DiscreteUniform, Laplace};
 
 #[allow(unused_must_use)]
-fn perform_pls(num_rows: usize) {
-    let feat_distr = Laplace::new(0.5, 5.).unwrap();
-    let target_distr = DiscreteUniform::new(0, 5).unwrap();
-    let num_feats: usize = 5;
-    let dataset = make_dataset(num_rows, num_feats, feat_distr, target_distr);
-    let targets = dataset.targets.into_shape((num_rows, 1)).unwrap();
-    let features = dataset.records;
-    let dataset = Dataset::new(features, targets);
-    let model = PlsRegression::params(3).scale(true).max_iterations(200);
+fn perform_pls(dataset: &Dataset<f64, f64>, alg: Algorithm) {
+    let model = PlsRegression::params(3)
+        .scale(true)
+        .max_iterations(200)
+        .algorithm(alg);
     model.fit(&dataset);
 }
 
 fn bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Linfa_pls");
-    let sizes: [usize; 3] = [1_000, 10_000, 100_000];
-    for size in sizes {
-        group.bench_with_input(BenchmarkId::new("Regression", size), &size, |b, size| {
-            b.iter(|| perform_pls(*size));
-        });
+    for (alg, name) in [(Algorithm::Nipals, "Nipals"), (Algorithm::Svd, "Svd")] {
+        let mut group = c.benchmark_group("Linfa_pls");
+        let sizes: [usize; 3] = [1_000, 10_000, 100_000];
+        
+        let feat_distr = Laplace::new(0.5, 5.).unwrap();
+        let target_distr = DiscreteUniform::new(0, 5).unwrap();
+
+        for size in sizes {
+            let dataset = make_dataset(size, 5, feat_distr, target_distr);
+            let targets = dataset.targets.into_shape((size, 1)).unwrap();
+            let features = dataset.records;
+            let dataset = Dataset::new(features, targets);
+            let input = (dataset, alg);
+            group.bench_with_input(BenchmarkId::new(name, size), &input, |b, (dataset, alg)| {
+                b.iter(|| perform_pls(dataset, *alg));
+            });
+        }
+        group.finish();
     }
-    group.finish();
 }
 
 criterion_group!(benches, bench);
