818ec1f68e07dd2e74fde4499cd725b5c0173a91
Femi Oho <47154698+oojo12@users.noreply.github.com>
2022-11-06 17:11:40 -0500

    pub use pls_generics

--- algorithms/linfa-pls/src/lib.rs
+++ algorithms/linfa-pls/src/lib.rs
@@ -37,9 +37,11 @@
 //! let exercices = array![[14., 146., 61.], [6., 80., 60.]];
 //! let physio_measures = pls.predict(exercices);
 //! ```
+//! 
 mod errors;
 mod hyperparams;
 mod pls_generic;
+pub use pls_generic::*;
 mod pls_svd;
 mod utils;
 
