bfe1ccf28f339c389d9fb063eddf0ed37de2aa7f
Femi Oho <47154698+oojo12@users.noreply.github.com>
2022-11-08 20:05:21 -0500

    fix build error

--- algorithms/linfa-linear/benches/ols_bench.rs
+++ algorithms/linfa-linear/benches/ols_bench.rs
@@ -4,14 +4,16 @@ use linfa_datasets::generate::make_dataset;
 use linfa_linear::{LinearRegression, TweedieRegressor};
 use statrs::distribution::{DiscreteUniform, Laplace};
 use linfa::Dataset;
+use ndarray::Ix1;
 
-
-fn perform_ols(dataset: &Dataset<f64, f64>) {
+#[allow(unused_must_use)]
+fn perform_ols(dataset: &Dataset<f64, f64, Ix1>) {
     let model = LinearRegression::new();
     model.fit(dataset);
 }
 
-fn perform_glm(dataset: &Dataset<f64, f64>) {
+#[allow(unused_must_use)]
+fn perform_glm(dataset: &Dataset<f64, f64, Ix1>) {
     let model = TweedieRegressor::params().power(0.).alpha(0.);
     model.fit(dataset);
 }
@@ -24,8 +26,8 @@ fn bench(c: &mut Criterion) {
     let feat_distr = Laplace::new(0.5, 5.).unwrap();
     let target_distr = DiscreteUniform::new(0, 5).unwrap();
 
-    let mut ols_id = "OLS".to_string();
-    let mut glm_id = "GLM".to_string();
+    let ols_id = "OLS-".to_string();
+    let glm_id = "GLM-".to_string();
 
     for size in sizes {
         for num_feat in num_feats {
@@ -34,12 +36,13 @@ fn bench(c: &mut Criterion) {
             func_name.push_str(&suffix);
             
             let dataset = make_dataset(size, num_feat, 1, feat_distr, target_distr);
+            let dataset = dataset.into_single_target();
 
             group.bench_with_input(
                 BenchmarkId::new(&func_name, size),
                 &dataset,
                 |b, dataset| {
-                    b.iter(|| perform_ols(dataset));
+                    b.iter(|| perform_ols(&dataset));
                 },
             );
 
@@ -49,7 +52,7 @@ fn bench(c: &mut Criterion) {
                 BenchmarkId::new(&func_name, size),
                 &dataset,
                 |b, dataset| {
-                    b.iter(|| perform_glm(dataset));
+                    b.iter(|| perform_glm(&dataset));
                 },
             );
         }
