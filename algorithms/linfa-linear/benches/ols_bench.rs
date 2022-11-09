c301cee082e3fa3947a260a612e0b6c64e97770a
Femi <47154698+oojo12@users.noreply.github.com>
2022-11-08 22:29:41 -0500

    Pls bench (#263)
    * Create ols_bench.rs
    
    * add bench
    
    * criterion benchmark
    
    * use make_dataset function
    
    * make return Dim compatiable with Fit methods
    
    * dev-depenences: add generate to linfa-datasets
    
    * omit dataset creation from bench timing
    
    * pub use pls_generics
    
    * added CCA and Canonical benches
    
    * reduce future count

--- algorithms/linfa-linear/benches/ols_bench.rs
+++ algorithms/linfa-linear/benches/ols_bench.rs
@@ -20,8 +20,7 @@ fn perform_glm(dataset: &Dataset<f64, f64, Ix1>) {
 
 fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("Linfa_linear");
-    let sizes: [usize; 3] = [1_000, 10_000, 100_000];
-    let num_feats: [usize; 2] = [5, 10];
+    let params: [(usize, usize); 4] = [(1_000, 5), (10_000, 5), (100_000, 5), (100_000, 10)];
 
     let feat_distr = Laplace::new(0.5, 5.).unwrap();
     let target_distr = DiscreteUniform::new(0, 5).unwrap();
@@ -29,33 +28,31 @@ fn bench(c: &mut Criterion) {
     let ols_id = "OLS-".to_string();
     let glm_id = "GLM-".to_string();
 
-    for size in sizes {
-        for num_feat in num_feats {
-            let suffix = format!("{}Feats", num_feat);
-            let mut func_name = ols_id.clone();
-            func_name.push_str(&suffix);
+    for (size, num_feat) in params {
+        let suffix = format!("{}Feats", num_feat);
+        let mut func_name = ols_id.clone();
+        func_name.push_str(&suffix);
 
-            let dataset = make_dataset(size, num_feat, 1, feat_distr, target_distr);
-            let dataset = dataset.into_single_target();
+        let dataset = make_dataset(size, num_feat, 1, feat_distr, target_distr);
+        let dataset = dataset.into_single_target();
 
-            group.bench_with_input(
-                BenchmarkId::new(&func_name, size),
-                &dataset,
-                |b, dataset| {
-                    b.iter(|| perform_ols(&dataset));
-                },
-            );
+        group.bench_with_input(
+            BenchmarkId::new(&func_name, size),
+            &dataset,
+            |b, dataset| {
+                b.iter(|| perform_ols(&dataset));
+            },
+        );
 
-            let mut func_name = glm_id.clone();
-            func_name.push_str(&suffix);
-            group.bench_with_input(
-                BenchmarkId::new(&func_name, size),
-                &dataset,
-                |b, dataset| {
-                    b.iter(|| perform_glm(&dataset));
-                },
-            );
-        }
+        let mut func_name = glm_id.clone();
+        func_name.push_str(&suffix);
+        group.bench_with_input(
+            BenchmarkId::new(&func_name, size),
+            &dataset,
+            |b, dataset| {
+                b.iter(|| perform_glm(&dataset));
+            },
+        );
     }
     group.finish();
 }
