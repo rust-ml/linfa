431f676415e0795814644b5d4322aa4fba8532c4
Femi Oho <47154698+oojo12@users.noreply.github.com>
2022-11-10 07:31:18 -0500

    address review

--- algorithms/linfa-linear/benches/ols_bench.rs
+++ algorithms/linfa-linear/benches/ols_bench.rs
@@ -61,13 +61,10 @@ fn bench(c: &mut Criterion) {
 
 #[cfg(not(target_os = "windows"))]
 criterion_group! {
-    name = not_win_benches;
+    name = benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
     targets = bench
 }
-#[cfg(not(target_os = "windows"))]
-criterion_main!(not_win_benches);
-#[cfg(target_os = "windows")]
-criterion_group!(win_benches, bench);
-#[cfg(target_os = "windows")]
-criterion_main!(win_benches);
+criterion_group!(benches, bench);
+
+criterion_main!(benches);
