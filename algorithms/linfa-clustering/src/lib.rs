ac127dd98600049b968f3f62673ab006284eeb95
Yuhan Lin <yuhanliin+github@protonmail.com>
2023-03-28 00:34:32 +0000

    Remove appx DBSCAN impl and replace with type aliases (#298)
    * Remove appx DBSCAN impl and replace with type aliases
    
    * Remove appx DBSCAN example and benchmark

--- algorithms/linfa-clustering/src/lib.rs
+++ algorithms/linfa-clustering/src/lib.rs
@@ -15,20 +15,28 @@
 //! Right now `linfa-clustering` provides the following clustering algorithms:
 //! * [K-Means](KMeans)
 //! * [DBSCAN](Dbscan)
-//! * [Approximated DBSCAN](AppxDbscan)
+//! * [Approximated DBSCAN](AppxDbscan) (Currently an alias for DBSCAN, due to its superior
+//! performance)
 //! * [Gaussian-Mixture-Model](GaussianMixtureModel)
 //! * [OPTICS](OpticsAnalysis)
 //!
 //! Implementation choices, algorithmic details and tutorials can be found in the page dedicated to the specific algorithms.
-mod appx_dbscan;
 mod dbscan;
 mod gaussian_mixture;
 #[allow(clippy::new_ret_no_self)]
 mod k_means;
 mod optics;
 
-pub use appx_dbscan::*;
 pub use dbscan::*;
 pub use gaussian_mixture::*;
 pub use k_means::*;
 pub use optics::*;
+
+// Approx DBSCAN is currently an alias for DBSCAN, due to the old Approx DBSCAN implementation's
+// lower performance and outdated dependencies
+
+use linfa_nn::distance::L2Dist;
+pub type AppxDbscanValidParams<F, N> = DbscanValidParams<F, L2Dist, N>;
+pub type AppxDbscanParams<F, N> = DbscanParams<F, L2Dist, N>;
+pub type AppxDbscanParamsError = DbscanParamsError;
+pub type AppxDbscan = Dbscan;
