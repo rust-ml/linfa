b273ccd43b53fb830d0567b7796e177110558ca1
relf <remi.lafage@onera.fr>
2025-05-05 20:57:25 +0200

    Revert lint to preserve MSRV

--- algorithms/linfa-trees/benches/decision_tree.rs
+++ algorithms/linfa-trees/benches/decision_tree.rs
@@ -39,8 +39,9 @@ fn decision_tree_bench(c: &mut Criterion) {
             Array2::random_using((n_classes, n_features), Uniform::new(-30., 30.), &mut rng);
 
         let train_x = generate_blobs(&centroids, *n, &mut rng);
+        #[allow(clippy::manual_repeat_n)]
         let train_y: Array1<usize> = (0..n_classes)
-            .flat_map(|x| std::iter::repeat_n(x, *n).collect::<Vec<usize>>())
+            .flat_map(|x| std::iter::repeat(x).take(*n).collect::<Vec<usize>>())
             .collect::<Array1<usize>>();
         let dataset = DatasetBase::new(train_x, train_y);
 
