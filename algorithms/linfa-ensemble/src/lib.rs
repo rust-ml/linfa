6a3699845ba44ba978317234d1ebd4fc623b147d
relf <remi.lafage@onera.fr>
2025-05-25 08:50:17 +0200

    Test accuracy of random forest and add sklearn doc ref

--- algorithms/linfa-ensemble/src/lib.rs
+++ algorithms/linfa-ensemble/src/lib.rs
@@ -1,14 +1,18 @@
-//! ## Ensemble Learning Algorithms
+//! # Ensemble Learning Algorithms
 //!
 //! Ensemble methods combine the predictions of several base estimators built with a given
 //! learning algorithm in order to improve generalizability / robustness over a single estimator.
 //!
 //! ## Random Forest
 //!
-//! Typical example of ensemble method is random forest, which combines the predictions of
-//! several decision trees trained on different parts of the same training set.
+//! A typical example of ensemble method is random forest, which combines the predictions of
+//! several decision trees (see `linfa-trees`) trained on different parts of the same training set.
 //!
-//! ### Example
+//! ## Reference
+//!
+//! * [Scikit-Learn User Guide](https://scikit-learn.org/stable/modules/ensemble.html)
+//!
+//! ## Example
 //!
 //! This example shows how to train a random forest model using 100 decision trees,
 //! each trained on 70% of the training data (bootstrap sampling).
@@ -42,3 +46,32 @@ mod hyperparams;
 
 pub use algorithm::*;
 pub use hyperparams::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use linfa::prelude::{Fit, Predict, ToConfusionMatrix};
+    use linfa_trees::DecisionTree;
+    use ndarray_rand::rand::SeedableRng;
+    use rand::rngs::SmallRng;
+
+    #[test]
+    fn test_ensemble_learner_accuracy_on_iris_dataset() {
+        let mut rng = SmallRng::seed_from_u64(42);
+        let (train, test) = linfa_datasets::iris()
+            .shuffle(&mut rng)
+            .split_with_ratio(0.8);
+
+        let model = EnsembleLearnerParams::new(DecisionTree::params())
+            .ensemble_size(100)
+            .bootstrap_proportion(0.7)
+            .fit(&train)
+            .unwrap();
+
+        let predictions = model.predict(&test);
+
+        let cm = predictions.confusion_matrix(&test).unwrap();
+        let acc = cm.accuracy();
+        assert!(acc > 0.9, "Expected accuracy to be above 90%, got {}", acc);
+    }
+}
