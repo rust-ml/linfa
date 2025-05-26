0cb64e576f33f945e1c3c65a249111c6fda24ce5
relf <remi.lafage@onera.fr>
2025-05-26 10:02:24 +0200

    Use bagging terminology

--- algorithms/linfa-ensemble/src/lib.rs
+++ algorithms/linfa-ensemble/src/lib.rs
@@ -3,10 +3,10 @@
 //! Ensemble methods combine the predictions of several base estimators built with a given
 //! learning algorithm in order to improve generalizability / robustness over a single estimator.
 //!
-//! ## Random Forest
+//! ## Bootstrap Aggregation (aka Bagging)
 //!
-//! A typical example of ensemble method is random forest, which combines the predictions of
-//! several decision trees (see `linfa-trees`) trained on different parts of the same training set.
+//! A typical example of ensemble method is Bootstrapo AGgregation, which combines the predictions of
+//! several decision trees (see `linfa-trees`) trained on different samples subset of the training dataset.
 //!
 //! ## Reference
 //!
@@ -14,7 +14,7 @@
 //!
 //! ## Example
 //!
-//! This example shows how to train a random forest model using 100 decision trees,
+//! This example shows how to train a bagging model using 100 decision trees,
 //! each trained on 70% of the training data (bootstrap sampling).
 //!
 //! ```no_run
@@ -30,15 +30,15 @@
 //!     .shuffle(&mut rng)
 //!     .split_with_ratio(0.8);
 //!
-//! // Train a random forest model on the iris dataset
-//! let random_forest_model = EnsembleLearnerParams::new(DecisionTree::params())
+//! // Train the model on the iris dataset
+//! let bagging_model = EnsembleLearnerParams::new(DecisionTree::params())
 //!     .ensemble_size(100)
 //!     .bootstrap_proportion(0.7)
 //!     .fit(&train)
 //!     .unwrap();
 //!
 //! // Make predictions on the test set
-//! let predictions = random_forest_model.predict(&test);
+//! let predictions = bagging_model.predict(&test);
 //! ```
 //!
 mod algorithm;
